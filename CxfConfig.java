9d897d42ec78b163f92917b02ed6d333122cea17
madhavan-sa <63124880+madhavan-sa@users.noreply.github.com>
2024-07-15 12:45:55 +0530

    Update CxfConfig.java

--- CxfConfig.java
+++ CxfConfig.java
@@ -1,8 +1,6 @@
 package com.example.demo.config;
 
-import com.example.demo.controller.FileUploadController;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
-import org.apache.cxf.jaxrs.swagger.Swagger2Feature;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,7 +12,6 @@ public class CxfConfig {
         JAXRSServerFactoryBean factoryBean = new JAXRSServerFactoryBean();
         factoryBean.setResourceClasses(FileUploadController.class);
         factoryBean.setAddress("/api");
-        factoryBean.getFeatures().add(new Swagger2Feature());
         return factoryBean;
     }
 }
